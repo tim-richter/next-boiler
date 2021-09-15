f8a1d05487b5da9fe89cc67d6b9a6bca1f43bab4
Tim <tim.chris.richter@gmail.com>
2021-09-15 22:30:48 +0200

    test: e2e tests

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -2,19 +2,17 @@ describe('Homepage', () => {
   beforeEach(() => {
     cy.visit('/');
   });
-  it('Brings header', () => {
-    cy.getBySel('main-heading').should('contain.text', 'superplate');
+
+  it('should have a header', () => {
+    cy.get('header').should('be.visible');
   });
 
-  it('Should have true href', () => {
-    // https://github.com/cypress-io/cypress-example-recipes/blob/master/examples/testing-dom__tab-handling-links/cypress/integration/tab_handling_anchor_links_spec.js
-    cy.getBySel('docs-btn-anchor')
-      .should('have.prop', 'href')
-      .and('equal', 'https://pankod.github.io/superplate/');
+  it('should have a main', () => {
+    cy.get('main').should('be.visible');
   });
 
-  it('Should have icons', () => {
-    cy.getBySel('icon').should('have.length', 6);
+  it('should have a footer', () => {
+    cy.get('footer').should('be.visible');
   });
 });
 
