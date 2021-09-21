176fa6cfbdba8b900a76b9cf9456dace57ac3403
Tim <tim.chris.richter@gmail.com>
2021-09-21 19:09:57 +0200

    style: add more global base styles

--- src/styles/global.css.ts
+++ src/styles/global.css.ts
@@ -1,6 +1,23 @@
 import { globalStyle } from '@vanilla-extract/css';
+import theme from '@styles/theme.css';
 
-globalStyle('body, body *', {
+globalStyle('body, body *, body *::before, body *::after', {
   boxSizing: 'border-box',
-  fontFamily: 'body',
+  fontFamily: theme.fontFamily.body,
+});
+
+globalStyle('body', {
+  backgroundColor: theme.color.black,
+  color: theme.color.white,
+  height: '100vh',
+  padding: 0,
+  margin: 0,
+});
+
+globalStyle('#__next', {
+  height: '100%',
+});
+
+globalStyle('main', {
+  height: '100%',
 });
