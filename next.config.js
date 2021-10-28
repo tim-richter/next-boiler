3a872c4e1b1bc4a14a2b7edeaae169ccefeb7523
Tim Richter <tim@timrichter.dev>
2021-10-28 17:05:46 +0200

    fix: remove next offline plugin (#5)

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
-const withOffline = require('next-offline');
 const { withSentryConfig } = require('@sentry/nextjs');
 const i18n = require('./next-i18next.config');
 
@@ -12,36 +11,11 @@ const i18n = require('./next-i18next.config');
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  transformManifest: (manifest) => ['/'].concat(manifest), // add the homepage to the cache
-
-  // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
-  // turn on the SW in dev mode so that we can actually test it
-  generateInDevMode: true,
-  workboxOpts: {
-    swDest: 'static/service-worker.js',
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'https-calls',
-          networkTimeoutSeconds: 15,
-          expiration: {
-            maxEntries: 150,
-            maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
-          },
-          cacheableResponse: {
-            statuses: [0, 200],
-          },
-        },
-      },
-    ],
-  },
   ...i18n,
 };
 
 const configWithPlugins = withPlugins(
-  [withOffline, createVanillaExtractPlugin(), withBundleAnalyzer],
+  [createVanillaExtractPlugin(), withBundleAnalyzer],
   nextConfig,
 );
 
