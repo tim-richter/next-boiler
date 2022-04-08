2ed0c9835ba6268d75541bf67b4e6c2abffba2d8
Tim <tim.chris.richter@gmail.com>
2022-04-08 21:02:35 +0200

    ci(local): add commitlint linting on git commits

---
+++ commitlint.config.js
@@ -0,0 +1,47 @@
+/**
+ * build: Changes that affect the build system or external dependencies
+ * ci: Changes to our CI configuration files and scripts
+ * docs: Documentation only changes
+ * feat: A new feature
+ * fix: A bug fix
+ * perf: A code change that improves performance
+ * refactor: A code change that neither fixes a bug nor adds a feature
+ * style: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
+ * test: Adding missing tests or correcting existing tests
+ */
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-leading-blank': [1, 'always'],
+    'body-max-line-length': [2, 'always', 100],
+    'footer-leading-blank': [1, 'always'],
+    'footer-max-line-length': [2, 'always', 100],
+    'header-max-length': [2, 'always', 100],
+    'scope-case': [2, 'always', 'lower-case'],
+    'subject-case': [
+      2,
+      'never',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+    ],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'style',
+        'test',
+      ],
+    ],
+  },
+};
