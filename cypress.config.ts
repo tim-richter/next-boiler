4fc62aee2dc512a5c49a75db1f7e96e32f766b15
Tim Richter <tim@timrichter.dev>
2022-07-06 15:02:19 +0200

    feat: upgrade all deps

---
+++ cypress.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      config.baseUrl = 'http://localhost:3000';
+
+      return config;
+    },
+  },
+});
--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -1,13 +1,13 @@
 /// <reference types="cypress" />
 
 import './commands';
-import 'cypress-axe';
 
 declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace Cypress {
     interface Chainable {
       getBySel(value: string): Chainable;
+      getBySelLike(value: string): Chainable;
     }
   }
 }
